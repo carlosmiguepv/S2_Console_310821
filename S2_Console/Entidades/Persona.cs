b19e31d8c360aad8ccc0f39b5a124c429b972785
carlos <carlos@LAPTOP-9Q6APE6P>
2021-10-15 02:55:55 -0500

    Add project files.

---
+++ S2_Console/Entidades/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S2_Console.Entidades
+{
+    public class Persona
+    {
+        public string dni
+        {
+            get;
+            set;
+        }
+
+        public string nombrecCompleto { get; set; }
+
+        public int telefono { get; set; }
+
+        public Persona()
+        {
+
+        }
+
+        public Persona(string dni, string nombreCompleto, int telefono)
+        {
+            this.dni = dni;
+            this.nombrecCompleto = nombrecCompleto;
+            this.telefono = telefono;
+        }
+    
+    
+    }
+}
---
+++ S2_Console/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using S2_Console.Entidades;
+
+namespace S2_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string nombreCurso = "Desarrollo de Aplicaciones Web 2021-2";
+            int edad = 20;
+            double monto = 200.01;
+
+            var nuevaEdad = "Veintiuno";
+            var peso = 70.50;
+
+            var persona = new Persona();
+        }
+    }
+}
